007c223d58691543d57170e5cd72d4cc0b11ba03
Bendeguz Szucs <benszucs@gmail.com>
2019-12-27 09:31:43 +0000

    solve pyramid both solutions

--- completed_exercises/pyramid/index.js
+++ completed_exercises/pyramid/index.js
@@ -35,21 +35,55 @@ function pyramid(n, row = 0, level = '') {
 }
 
 module.exports = pyramid;
+
+
+
+// 1. iterative solution
 //
-// function pyramid(n) {
-//   const midpoint = Math.floor((2 * n - 1) / 2);
+// from 0 to n (iterate through rows)
+//      create an empty string, 'level'
+//      From 0 to ??? (columns)
+//          If the column should have #
+//              Add a # to 'level'
+//          Else
+//              Add a space to 'level'
+//      console log 'stair
 //
-//   for (let row = 0; row < n; row++) {
-//     let level = '';
+// Solution:
+// const midpoint = Math.floor((2*n -1) / 2)
 //
-//     for (let column = 0; column < 2 * n - 1; column++) {
-//       if (midpoint - row <= column && midpoint + row >= column) {
-//         level += '#';
-//       } else {
-//         level += ' ';
+// for (let row = 0; row < n; row++) {
+//       let level = '';
+//
+//       for (let column = 0; column < 2*n - 1; column++) {
+//           if (midpoint - row <= column && midpoint + row >= column) {
+//               level += '#'
+//           } else {
+//               level += ' '
+//           }
 //       }
-//     }
+//       console.log(level)
+// }
+
+
+// 2. Recursive solution
 //
-//     console.log(level);
-//   }
+// function pyramid(n, row = 0, level = '') {
+// 	let column = 2 * n - 1;
+// 	let midpoint = Math.floor((2 * n - 1) / 2);
+// 	if (n === row) {
+// 		return;
+// 	}
+//
+// 	if (level.length === 2 * n - 1) {
+// 		console.log(level);
+// 		return pyramid(n, row + 1);
+// 	}
+//
+// 	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+// 		level += '#';
+// 	} else {
+// 		level += ' ';
+// 	}
+// 	return pyramid(n, row, level);
 // }
